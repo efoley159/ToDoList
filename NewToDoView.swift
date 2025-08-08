06db59b93a780127a806c2854a28579a8896d05a
Elaina Foley <emtfoley1@Gmail.com>
2025-08-08 13:17:59 -0700

    added a save button

---
+++ NewToDoView.swift
@@ -0,0 +1,33 @@
+//
+//  NewToDoView.swift
+//  ToDoList
+//
+//  Created by Scholar on 8/8/25.
+//
+
+import SwiftUI
+
+struct NewToDoView: View {
+    var body: some View {
+        VStack {
+            Text("Task Title")
+            TextField("Enter the task description...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .padding(.leading)
+            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                Text("Is it important?")
+            }
+            Button {
+
+            } label: {
+                Text("Save")
+                    
+            }
+
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    NewToDoView()
+}
